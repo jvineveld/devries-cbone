00fd7aff4a3567b10d83a4a1006f1da10afe56c8
Daniel <daanf@live.nl>
2018-08-21 16:24:44 +0200

    Fixed the api request. Now working.

--- Controller/Json/Multiple.php
+++ Controller/Json/Multiple.php
@@ -14,9 +14,8 @@ class Multiple extends \Xlii\CbOne\Controller\Json
 
     public function execute()
     {
-        $data = $this->getRequest()->getParam('data');
-        $data = array("9789400406254" => 8045, "9789044352894" => 80, "9789041422903" => 8045);
+        $data = $this->getRequest()->getPostValue();
         echo json_encode($this->getAvailability($data));
-        die();;
+        die();
     }
 }
