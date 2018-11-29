a3f007234edc433501a4c273607bbca1d6b8cde4
Bart de Vries <bartdevries@MacBook-Pro-6.dynamic.ziggo.nl>
2018-11-29 21:43:43 +0100

    no 24 hour delivery in weekends and on holidays

--- Controller/Json.php
+++ Controller/Json.php
@@ -16,6 +16,14 @@ abstract class Json extends \Magento\Framework\App\Action\Action
     {
         $body = array();
         $body['Product'] = array();
+        if(!$this->isWorkDay())
+        {
+            foreach($productIds as $returnProductId => $qty)
+            {
+                    $returnArray[$returnProductId] = 0;
+            }
+            return $returnArray;
+        }
         foreach($productIds as $productId => $qty)
         {
             $product = ['ProductId' => $productId, "ProductIdType" => "EAN"];
@@ -44,5 +52,61 @@ abstract class Json extends \Magento\Framework\App\Action\Action
             }
         }
         return $returnArray;
+    }
+
+    public function isWorkDay()
+    {
+        if(date('N') >= 6)
+            return false;
+
+        $date = date('d-m-Y');
+        foreach($this->getHolidays() as $holiday)
+        {
+            if($date == $holiday)
+            {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public function getHolidays()
+    {
+        $holidays = array();
+        for ($i=0;$i<10;$i++) {
+            $holidays[] = $this->_holidays(date('Y'),$i);
+        }
+        return $holidays;
+    }
+
+    private function _holidays($jaar,$feestdag){
+        $a = $jaar % 19;
+        $b = intval($jaar/100);
+        $c = $jaar % 100;
+        $d = intval($b/4);
+        $e = $b % 4;
+        $g = intval((8 *  $b + 13) / 25);
+        $theta = intval((11 * ($b - $d - $g) - 4) / 30);
+        $phi = intval((7 * $a + $theta + 6) / 11);
+        $psi = (19 * $a + ($b - $d - $g) + 15 -$phi) % 29;
+        $i = intval($c / 4);
+        $k = $c % 4;
+        $lamda = ((32 + 2 * $e) + 2 * $i - $k - $psi) % 7;
+        $maand = intval((90 + ($psi + $lamda)) / 25);
+        $dag = (19 + ($psi + $lamda) + $maand) % 32;
+        Switch( $feestdag ){
+            Case 0: $datumfeestdag = mktime (1,1,1,1,1,$jaar); break;            // Nieuwjaarsdag
+            Case 1: $datumfeestdag = mktime (0,0,0,$maand,$dag-2,$jaar); break;  // Goede Vrijdag
+            Case 2: $datumfeestdag = mktime (0,0,0,$maand,$dag,$jaar); break;    // 1e Paasdag
+            Case 3: $datumfeestdag = mktime (0,0,0,$maand,$dag+1,$jaar); break;  // 2e Paasdag
+            Case 4: $datumfeestdag = mktime (0,0,0,4,27,$jaar); break;           // Koningsdag
+            Case 5: $datumfeestdag = mktime (0,0,0,$maand,$dag+39,$jaar); break; // Hemelvaart
+            Case 6: $datumfeestdag = mktime (0,0,0,$maand,$dag+49,$jaar); break; // 1e Pinksterdag
+            Case 7: $datumfeestdag = mktime (0,0,0,$maand,$dag+50,$jaar); break; // 2e Pinksterdag
+            Case 8: $datumfeestdag = mktime (0,0,0,12,25,$jaar); break;          // 1e Kerstdag
+            Case 9: $datumfeestdag = mktime (0,0,0,12,26,$jaar); break;          // 2e Kerstdag
+        }
+        Return Date("d-m-Y",$datumfeestdag);
     }
 }
